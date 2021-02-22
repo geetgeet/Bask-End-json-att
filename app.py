a12d7c17f9f4f05cfbe4e568266556cdf6a55485
geetgeet <gmoses728@gmail.com>
2021-02-22 14:56:30 +0200

    init

--- app.py
+++ app.py
@@ -1,39 +1,57 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+
+
 def init_sqlite_db():
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
-    conn.execute('CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, city TEXT, pin_code TEXT)')
+    conn.execute('CREATE TABLE IF NOT EXISTS Items (id INTEGER PRIMARY KEY AUTOINCREMENT, product_name TEXT,price  TEXT, brand TEXT, picture BLOB)')
     print("Table created successfully")
     conn.close()
 init_sqlite_db()
 app = Flask(__name__)
 @app.route('/')
-@app.route('/registration-form/')
+@app.route('/add-product/')
 def enter_new_student():
-    return render_template('student.html')
+    return render_template('new-product.html')
 @app.route('/add-new-record/', methods=['POST'])
 def add_new_record():
     if request.method == "POST":
         msg = None
         try:
-            name = request.form['name']
-            address = request.form['address']
-            city = request.form['city']
-            pin_code = request.form['pin-code']
+            product_name = request.form['product_name']
+            price = request.form['price']
+            brand = request.form['brand']
+            picture = request.form['picture']
+
+
 
             with sqlite3.connect('database.db') as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO student (name, address, city, pin_code) VALUES (?, ?, ?, ?)", (name, address, city, pin_code))
+                cur.execute("INSERT INTO Items (product_name, price, brand, picture) VALUES (?, ?, ?, ?)", (product_name, price, brand, picture))
                 con.commit()
-                msg = name + " was successfully added to the database."
+                msg = product_name + " was successfully added to the database."
         except Exception as e:
             con.rollback()
             msg = "Error occurred in insert operation: " + str(e)
         finally:
             con.close()
             return render_template('result.html', msg=msg)
+@app.route('/show-records-table/', methods=["GET"])
 
+def show_table():
+    records = []
+    try:
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM Items")
+            records = cur.fetchall()
+    except Exception as e:
+        con.rollback()
+        print("There was an error fetching results from the database: " + str(e))
+    finally:
+        con.close()
+        return render_template('records.html')
 
 @app.route('/show-records/', methods=["GET"])
 def show_records():
@@ -41,24 +59,51 @@ def show_records():
     try:
         with sqlite3.connect('database.db') as con:
             cur = con.cursor()
-            cur.execute("SELECT * FROM student")
+            cur.execute("SELECT * FROM Items")
             records = cur.fetchall()
     except Exception as e:
         con.rollback()
         print("There was an error fetching results from the database: " + str(e))
     finally:
         con.close()
-        return render_template('records.html', records=records)
+        d=records
+        return jsonify(d)
 
 
-@app.route('/delete-student/<int:student_id>/', methods=["GET"])
-def delete_student(student_id):
+
+
+
+
+
+
+@app.route('/edit-item/<int:product_id>/',methods=['GET'])
+def edit_product(product_id):
+    return render_template('edit.html')
+    msg="Edited"
+    try:
+
+        with sqlite3.connect('database.db') as con:
+
+            cur = con.cursor()
+            cur.execute("UPDATE Items SET (product_name, price, brand, picture) VALUES (?, ?, ?, ?) WHERE id=" + str(product_id))
+            con.commit()
+            msg = "A record was edited successfully from the database."
+    except Exception as e:
+            con.rollback()
+            msg = "Error occurred when editing an item in the database: " + str(e)
+    finally:
+            con.close()
+            return render_template('delete-success.html', msg=msg)
+
+
+@app.route('/delete-item/<int:product_id>/', methods=["GET"])
+def delete_product(product_id):
 
     msg = None
     try:
         with sqlite3.connect('database.db') as con:
             cur = con.cursor()
-            cur.execute("DELETE FROM student WHERE id=" + str(student_id))
+            cur.execute("DELETE FROM Items WHERE id=" + str(product_id))
             con.commit()
             msg = "A record was deleted successfully from the database."
     except Exception as e:
